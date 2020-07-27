2c84e2ab6eacebd882c3dcca61cbbbd45a901fae
coderDee <66824038+CoderDee@users.noreply.github.com>
2020-07-27 16:53:16 +0300

    sdsd

--- js/js.js
+++ js/js.js
@@ -24,6 +24,14 @@ AOS.init({
 });
 
 
+
+
+
+//галерея гаджетов======================================================================
+
+
+
+
 // кнопка наверх============================================
 
 (function() {
@@ -111,3 +119,65 @@ function consoleText(words, id, colors) {
   }, 400)
 }
 
+// 3d карточки
+const cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove', startRotate);
+    card.addEventListener('mouseout', stopRotate);
+}
+
+function startRotate(event) {
+    const cardItem = this.querySelector('.card-item');
+    const halfHeight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+    cardItem.style.transform = 'rotateX(' + - (event.offsetY - halfHeight) / 8 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 8 + 'deg)';
+}
+
+function stopRotate(event) {
+    const cardItem = this.querySelector('.card-item');
+    cardItem.style.transform = 'rotate(0)';
+}
+
+
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
